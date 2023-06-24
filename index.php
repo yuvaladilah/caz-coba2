fd7f4432cb8fcf6a35c3d16fd3698b8c3bb3ebd0
yuvaladilah <yuvaladilah0@gmail.com>
2023-06-24 12:52:50 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>AING MAUNG</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quae soluta rerum debitis mollitia eos dolores molestias quibusdam eveniet autem, nostrum beatae facere, ea commodi iure repudiandae, culpa nihil natus tempora molestiae error dicta ex omnis. Voluptates eligendi recusandae corporis mollitia ad repellendus cumque minus nobis expedita laborum maxime fuga unde perferendis, ex possimus rem numquam excepturi dolore! Error laudantium vel qui dolores eum similique veritatis deserunt. Officia fugit deleniti voluptatum voluptate, eos quibusdam veniam adipisci aperiam inventore laboriosam magni placeat obcaecati incidunt, natus consectetur facilis dignissimos hic unde nihil. Fuga quisquam consequuntur non porro reprehenderit eum nostrum ad debitis.</p>
 </body>
+
 </html>
