52422716f11eab8e52c72acdf1c3a5081ceeb033
yuvaladilah <yuvaladilah0@gmail.com>
2023-06-24 12:59:13 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>AING MAUNG</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quae soluta rerum debitis mollitia eos dolores molestias quibusdam eveniet autem, nostrum beatae facere, ea commodi iure repudiandae, culpa nihil natus tempora molestiae error dicta ex omnis. Voluptates eligendi recusandae corporis mollitia ad repellendus cumque minus nobis expedita laborum maxime fuga unde perferendis, ex possimus rem numquam excepturi dolore! Error laudantium vel qui dolores eum similique veritatis deserunt. Officia fugit deleniti voluptatum voluptate, eos quibusdam veniam adipisci aperiam inventore laboriosam magni placeat obcaecati incidunt, natus consectetur facilis dignissimos hic unde nihil. Fuga quisquam consequuntur non porro reprehenderit eum nostrum ad debitis.</p>
+    <ul></ul>
+    <ul>
+        <li>yuval : 152021121</li>
+    </ul>
 </body>
 
 </html>
